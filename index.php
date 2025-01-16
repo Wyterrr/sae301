d8cff434b640f9c204c9a307211c96c0375e4f77
Phalc0 _ <pierreintvg.pro@gmail.com>
2025-01-16 10:30:18 +0100

    ajout de la balise script

--- index.php
+++ index.php
@@ -80,6 +80,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="script.js" defer></script>
     <title>Ajouter une Commande</title>
 </head>
 <body>
